4b9e9bb9778794b1991c8a9e55d9319885fa6903
jonathanfb <jonathanfb@jonathanfb-Lenovo-B40-80>
2020-08-22 01:16:32 -0500

    Operacion Suma utilizando Array y Scanner , mostrar de igual manera suma total es: de los numeros que asigne el usario.

---
+++ src/operacion/suma/OperacionSuma.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacion.suma;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jonathanfb
+ */
+public class OperacionSuma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner num = new Scanner(System.in);
+
+        int totalsuma = 0;
+        int[] numeros = new int[4];
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            System.out.println("Digite los numeros que quiera presentar en la serie");
+            numeros[i] = num.nextInt();
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            totalsuma = totalsuma + numeros[i];
+
+        }
+
+        System.out.println(" Suma Total es: "+ totalsuma);
+
+        // TODO code application logic here
+    }
+
+}
